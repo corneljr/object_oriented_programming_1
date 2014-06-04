b9434202a7688a15de11194b0351d6a4eb0d7434
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-04 18:12:09 -0400

    refactored mars rover exercise

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,33 +1,44 @@
-class Rover
+class Plateau
+	attr_reader :height, :width
+	def initialize(height, width)
+		@height = height
+		@width = width
+	end
+end
 
-	def initialize(x,y,orientation)
+class Rover
+	def initialize(x,y,orientation,plateau)
 		@x = x
 		@y = y
 		@orientation = orientation
+		@plateau = plateau
 	end
 
 	def instructions(str)
-		 moves = str.split("")
-		 moves.each do |move|
-		 	if (move == "L") || (move == "R") 
-		 		rotate(move)
-		 	else
+		 str.each_char do |move|
+		 	if move == 'M'
 		 		forward
+		 	else
+		 		rotate(move)
 		 	end
 		 end
-		 puts "#{@x} #{@y} #{@orientation}"
+		 result
+	end
+
+	def result
+		 if (0..@plateau.height).include?(@x) && (0..@plateau.width).include?(@y)
+		 	puts "#{@x} #{@y} #{@orientation}" 
+		 else
+		 	puts "Rover has died"
+		 end
 	end
 
 	def forward
 		case @orientation
-		when "N"
-			@y += 1
-		when "E"
-			@x += 1
-		when "W"
-			@x -= 1
-		when "S"
-			@y -= 1
+		when "N" then @y += 1
+		when "E" then @x += 1
+		when "W" then @x -= 1
+		when "S" then @y -= 1
 		end
 	end
 
@@ -35,14 +46,15 @@ class Rover
 	def rotate(direction)
 		directions = ["W","N","E","S"]
 
-		if direction == "L"
-			@orientation = directions[directions.index(@orientation) - 1]
-		elsif direction == "R"
-			@orientation = directions[directions.reverse.index(@orientation) - 1]
-		end
-		@orientation
+		directions.reverse! if direction == "R"
+		@orientation = directions[directions.index(@orientation) - 1]
 	end
 end
 
-rover = Rover.new(1,2,"N")
+mars = Plateau.new(5,5)
+
+rover = Rover.new(1,2,"N",mars)
 rover.instructions("LMLMLMLMM")
+
+rover = Rover.new(8,9,"E",mars)
+rover.instructions("MMRMMRMRRM")
